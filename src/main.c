f27831c217d599af1eff82c14fc41fb96de2f9bb
Vinícius Daniel <dannful@gmail.com>
2025-09-05 20:27:57 -0300

    Add command-line argument parsing with argp

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <argp.h>
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,7 +9,95 @@
 #include "setup.h"
 #include "worker.h"
 
+static struct argp_option options[] = {
+    {"size_x", 128, "INTEGER", 0, "Grid size in X"},
+    {"size_y", 129, "INTEGER", 0, "Grid size in Y"},
+    {"size_z", 130, "INTEGER", 0, "Grid size in Z"},
+    {"dx", 131, "FLOAT", 0, "Step size in X"},
+    {"dy", 132, "FLOAT", 0, "Step size in Y"},
+    {"dz", 133, "FLOAT", 0, "Step size in Z"},
+    {"dt", 134, "FLOAT", 0, "Step size in time"},
+    {"time_max", 't', "FLOAT", 0, "Max time"},
+    {"iterations", 'i', "INTEGER", 0, "Number of iterations"},
+    {"stencil_size", 's', "INTEGER", 0, "Stencil size"}};
+
+struct arguments {
+  size_t size_x;
+  size_t size_y;
+  size_t size_z;
+  float dx;
+  float dy;
+  float dz;
+  float dt;
+  float time_max;
+  unsigned int iterations;
+  size_t stencil_size;
+};
+
+static error_t parse_opt(int key, char *arg, struct argp_state *state) {
+  struct arguments *arguments = state->input;
+  switch (key) {
+  case 130:
+    arguments->size_x = atoi(arg);
+    break;
+  case 131:
+    arguments->size_y = atoi(arg);
+    break;
+  case 132:
+    arguments->size_z = atoi(arg);
+    break;
+  case 133:
+    arguments->dx = atof(arg);
+    break;
+  case 134:
+    arguments->dy = atof(arg);
+    break;
+  case 135:
+    arguments->dz = atof(arg);
+    break;
+  case 136:
+    arguments->dt = atof(arg);
+    break;
+  case 't':
+    arguments->time_max = atof(arg);
+    break;
+  case 'i':
+    arguments->iterations = atoi(arg);
+    break;
+  case 's':
+    arguments->stencil_size = atoi(arg);
+    break;
+  case ARGP_KEY_END:
+    if (arguments->size_x == 0 || arguments->size_y == 0 ||
+        arguments->size_z == 0 || arguments->dx == 0 || arguments->dy == 0 ||
+        arguments->dz == 0 || arguments->iterations == 0 ||
+        arguments->time_max == 0 || arguments->dt == 0) {
+      argp_usage(state);
+    }
+    break;
+  default:
+    return ARGP_ERR_UNKNOWN;
+  }
+  return 0;
+}
+
+static struct argp argp = {
+    options, parse_opt, NULL,
+    "A program that solves Fletcher equations in a distributed setup"};
+
 int main(int argc, char **argv) {
+  struct arguments arguments;
+  arguments.size_x = 0;
+  arguments.size_y = 0;
+  arguments.size_z = 0;
+  arguments.dx = 0;
+  arguments.dy = 0;
+  arguments.dz = 0;
+  arguments.dt = 0;
+  arguments.time_max = 0;
+  arguments.iterations = 0;
+  arguments.stencil_size = 0;
+  argp_parse(&argp, argc, argv, 0, 0, &arguments);
   MPI_Comm communicator;
   int topology[DIMENSIONS] = {0};
   int rank, size;
@@ -20,18 +109,14 @@ int main(int argc, char **argv) {
 
   dc_process_t mpi_process = dc_process_init(communicator, rank, topology);
 
-  const size_t size_x = atoi(argv[1]);
-  const size_t size_y = atoi(argv[2]);
-  const size_t size_z = atoi(argv[3]);
-  const unsigned int iterations = atoi(argv[4]), stencil_size = atoi(argv[5]);
-
   double start_time = MPI_Wtime();
 
   if (rank == COORDINATOR) {
     dc_log_info(rank, "Initializing problem data...");
-    problem_data_t problem_data =
-        dc_initialize_problem(communicator, (unsigned int *)topology, size,
-                              iterations, stencil_size, size_x, size_y, size_z);
+    problem_data_t problem_data = dc_initialize_problem(
+        communicator, (unsigned int *)topology, size, arguments.iterations,
+        arguments.stencil_size, arguments.size_x, arguments.size_y,
+        arguments.size_z);
     dc_log_info(rank, "Partitioning cube...");
     dc_partition_cube(problem_data);
     dc_log_info(rank, "Partition completed. Sending data to workers...");
@@ -57,9 +142,9 @@ int main(int argc, char **argv) {
   dc_worker_process(mpi_process);
   dc_send_data_to_coordinator(mpi_process);
   if (rank == COORDINATOR) {
-    size_t total_size = size_x * size_y * size_z;
-    float *cube =
-        dc_receive_data_from_workers(mpi_process, size_x, size_y, size_z);
+    size_t total_size = arguments.size_x * arguments.size_y * arguments.size_z;
+    float *cube = dc_receive_data_from_workers(
+        mpi_process, arguments.size_x, arguments.size_y, arguments.size_z);
     FILE *output = fopen(argv[6], "wb");
     fwrite(cube, sizeof(float), total_size, output);
     fclose(output);

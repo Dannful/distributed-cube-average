5797e2c180bb69bcb83bb0a59110d4a57a24b9f3
Vinícius Daniel <dannful@gmail.com>
2026-03-24 13:40:24 -0300

    Incremented thesis

--- thesis/flake.nix
+++ thesis/flake.nix
@@ -2,16 +2,32 @@
   inputs = {
     utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          texliveFull
-        ];
-      };
-    }
-  );
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        thesis = pkgs.writeShellScriptBin "build-latex" ''
+          pdflatex *.tex
+          bibtex *.aux
+          pdflatex *.tex
+          pdflatex *.tex
+        '';
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            texliveFull
+          ];
+        };
+        apps = {
+          default = {
+            type = "app";
+            program = pkgs.lib.getExe thesis;
+          };
+        };
+      }
+    );
 }

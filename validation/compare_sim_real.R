f76a09570f2f12b867e8d091107657d97e2ba133
Vinícius Daniel <dannful@gmail.com>
2026-05-04 14:38:23 -0300

    Included additional charts

--- validation/compare_sim_real.R
+++ validation/compare_sim_real.R
@@ -3,6 +3,7 @@ library(dplyr)
 library(stringr)
 library(purrr)
 library(ggplot2)
+library(patchwork)
 
 args <- commandArgs(trailingOnly = TRUE)
 
@@ -75,17 +76,14 @@ load_real_traces <- function(traces_path) {
   bind_rows(df)
 }
 
-generate_combined_gantt_chart <- function(real_df, sim_df) {
-  combined_df <- bind_rows(list(real = real_df, sim = sim_df), .id = "src") |>
-    mutate(src = as.factor(src)) |> filter(Start < 3)
-  combined_df |>
+generate_gantt_chart <- function(df) {
+  df |>
     ggplot() +
     geom_rect(aes(
       xmin = Start, xmax = End, ymin = Rank - 0.5,
       ymax = Rank + 0.5, fill = Operation
     )) +
     scale_y_continuous(breaks = scales::breaks_width(1)) +
-    facet_wrap(~src, labeller = as_labeller(c("real" = "Real", "sim" = "Simulation"))) +
     xlab("Time [seconds]") +
     ylab("Rank") +
     theme_bw(base_size = 14) +
@@ -96,6 +94,166 @@ generate_combined_gantt_chart <- function(real_df, sim_df) {
         0,
         "pt"
       ), legend.box.margin = margin(0, 0, 0, 0), legend.title = element_text(size = 10)
+    )
+}
+
+generate_combined_gantt_chart <- function(real_df, sim_df) {
+  bind_rows(list(real = real_df, sim = sim_df), .id = "src") |>
+    mutate(src = as.factor(src)) |>
+    filter(Start < 3) |>
+    generate_gantt_chart() +
+    facet_wrap(~src, labeller = as_labeller(c("real" = "Real", "sim" = "Simulation")))
+}
+
+compute_load_data <- function(df, n_slices = 100) {
+  t_min <- min(df$Start)
+  t_max <- max(df$End)
+  slice_width <- (t_max - t_min) / n_slices
+  slices <- tibble(
+    Slice = seq_len(n_slices),
+    StartTS = t_min + (seq_len(n_slices) - 1) * slice_width,
+    EndTS = t_min + seq_len(n_slices) * slice_width,
+    DurationTS = slice_width
+  )
+  df |>
+    cross_join(slices) |>
+    mutate(Time.Sum = pmax(0, pmin(End, EndTS) - pmax(Start, StartTS))) |>
+    filter(Time.Sum > 0) |>
+    group_by(Rank, Slice, StartTS, EndTS, DurationTS, Operation) |>
+    summarise(Time.Sum = sum(Time.Sum), .groups = "drop")
+}
+
+generate_load_chart <- function(df) {
+  df |>
+    filter(Time.Sum != 0) |>
+    group_by(Slice, StartTS, EndTS, DurationTS, Operation) |>
+    summarize(N = n(), Time.Sum = sum(Time.Sum), .groups = "drop_last") |>
+    ungroup(Operation) |>
+    arrange(Operation) |>
+    mutate(TaskHeight = Time.Sum / (DurationTS * N)) |>
+    mutate(TaskPosition = cumsum(TaskHeight) - TaskHeight) |>
+    ggplot() +
+    coord_cartesian(ylim = c(0, 1)) +
+    geom_rect(alpha = 0.5, aes(
+      fill = Operation,
+      xmin = StartTS, xmax = EndTS,
+      ymin = TaskPosition, ymax = TaskPosition + TaskHeight
+    )) +
+    ylab("Load [%]") +
+    xlab("Time [seconds]") +
+    theme_bw(base_size = 14) +
+    theme(
+      panel.grid = element_blank(),
+      legend.position = "top", legend.justification = "left",
+      legend.box.spacing = unit(0, "pt"),
+      legend.box.margin = margin(0, 0, 0, 0),
+      legend.title = element_text(size = 10)
+    )
+}
+
+generate_combined_load_chart <- function(real_df, sim_df) {
+  bind_rows(
+    list(real = compute_load_data(real_df), sim = compute_load_data(sim_df)),
+    .id = "src"
+  ) |>
+    mutate(src = as.factor(src)) |>
+    filter(Time.Sum != 0) |>
+    group_by(src, Slice, StartTS, EndTS, DurationTS, Operation) |>
+    summarize(N = n(), Time.Sum = sum(Time.Sum), .groups = "drop_last") |>
+    ungroup(Operation) |>
+    arrange(Operation) |>
+    mutate(TaskHeight = Time.Sum / (DurationTS * N)) |>
+    mutate(TaskPosition = cumsum(TaskHeight) - TaskHeight) |>
+    ggplot() +
+    coord_cartesian(ylim = c(0, 1)) +
+    geom_rect(alpha = 0.5, aes(
+      fill = Operation,
+      xmin = StartTS, xmax = EndTS,
+      ymin = TaskPosition, ymax = TaskPosition + TaskHeight
+    )) +
+    facet_wrap(~src, labeller = as_labeller(c("real" = "Real", "sim" = "Simulation"))) +
+    ylab("Load [%]") +
+    xlab("Time [seconds]") +
+    theme_bw(base_size = 14) +
+    theme(
+      panel.grid = element_blank(),
+      legend.position = "top", legend.justification = "left",
+      legend.box.spacing = unit(0, "pt"),
+      legend.box.margin = margin(0, 0, 0, 0),
+      legend.title = element_text(size = 10)
+    )
+}
+
+generate_rank_chart <- function(df) {
+  yconfm <- df |>
+    distinct(Rank) |>
+    arrange(Rank) |>
+    mutate(Position = 0:(n() - 1))
+  df |>
+    filter(Time.Sum != 0) |>
+    group_by(Rank, Slice, StartTS, EndTS, DurationTS, Operation) |>
+    summarize(Time.Sum = sum(Time.Sum), .groups = "drop_last") |>
+    ungroup(Operation) |>
+    arrange(Operation) |>
+    mutate(TaskHeight = Time.Sum / DurationTS) |>
+    mutate(TaskPosition = cumsum(TaskHeight) - TaskHeight) |>
+    left_join(yconfm, by = "Rank") |>
+    ggplot() +
+    scale_y_continuous(breaks = yconfm$Position, labels = yconfm$Rank) +
+    geom_rect(alpha = 0.5, aes(
+      fill = Operation,
+      xmin = StartTS, xmax = EndTS,
+      ymin = Position + TaskPosition,
+      ymax = Position + TaskPosition + TaskHeight
+    )) +
+    ylab("Rank [index]") +
+    xlab("Time [seconds]") +
+    theme_bw(base_size = 14) +
+    theme(
+      panel.grid = element_blank(),
+      legend.position = "top", legend.justification = "left",
+      legend.box.spacing = unit(0, "pt"),
+      legend.box.margin = margin(0, 0, 0, 0),
+      legend.title = element_text(size = 10)
+    )
+}
+
+generate_combined_rank_chart <- function(real_df, sim_df) {
+  combined <- bind_rows(
+    list(real = compute_load_data(real_df), sim = compute_load_data(sim_df)),
+    .id = "src"
+  ) |> mutate(src = as.factor(src))
+  yconfm <- combined |>
+    distinct(Rank) |>
+    arrange(Rank) |>
+    mutate(Position = 0:(n() - 1))
+  combined |>
+    filter(Time.Sum != 0) |>
+    group_by(src, Rank, Slice, StartTS, EndTS, DurationTS, Operation) |>
+    summarize(Time.Sum = sum(Time.Sum), .groups = "drop_last") |>
+    ungroup(Operation) |>
+    arrange(Operation) |>
+    mutate(TaskHeight = Time.Sum / DurationTS) |>
+    mutate(TaskPosition = cumsum(TaskHeight) - TaskHeight) |>
+    left_join(yconfm, by = "Rank") |>
+    ggplot() +
+    scale_y_continuous(breaks = yconfm$Position, labels = yconfm$Rank) +
+    geom_rect(alpha = 0.5, aes(
+      fill = Operation,
+      xmin = StartTS, xmax = EndTS,
+      ymin = Position + TaskPosition,
+      ymax = Position + TaskPosition + TaskHeight
+    )) +
+    facet_wrap(~src, labeller = as_labeller(c("real" = "Real", "sim" = "Simulation"))) +
+    ylab("Rank [index]") +
+    xlab("Time [seconds]") +
+    theme_bw(base_size = 14) +
+    theme(
+      panel.grid = element_blank(),
+      legend.position = "top", legend.justification = "left",
+      legend.box.spacing = unit(0, "pt"),
+      legend.box.margin = margin(0, 0, 0, 0),
+      legend.title = element_text(size = 10)
     )
 }
 
@@ -153,6 +311,23 @@ summarized_dataset <- function(df) {
     relocate(problem_size, compute_time, mpi_time, total_time)
 }
 
+save_load_charts <- function(real_dataset, simulation_dataset) {
+  walk(problem_sizes, \(size) {
+    base_dir <- "plots"
+    dir.create(base_dir, showWarnings = FALSE)
+    real_filtered <- real_dataset |> filter(problem_size == size)
+    sim_filtered <- simulation_dataset |> filter(problem_size == size)
+    p_rank <- generate_combined_rank_chart(real_filtered, sim_filtered) +
+      theme(axis.title.x = element_blank(), axis.text.x = element_blank())
+    p_load <- generate_combined_load_chart(real_filtered, sim_filtered) +
+      theme(legend.position = "none", strip.text = element_blank(), strip.background = element_blank())
+    plot <- p_rank / p_load + plot_layout(heights = c(1, 0.15))
+    pdf_path <- paste0(base_dir, "/load_", size, ".pdf")
+    ggsave(pdf_path, plot, width = 15, height = 8)
+    print(paste0("Saving ", pdf_path, "..."))
+  })
+}
+
 save_gantt_charts <- function(real_dataset, simulation_dataset) {
   walk(problem_sizes, \(size) {
     base_dir <- "plots"
@@ -179,6 +354,7 @@ simulation_dataset <- load_simulation(sim_traces_path) |>
   simplify_dataset()
 
 save_gantt_charts(real_dataset, simulation_dataset)
+save_load_charts(real_dataset, simulation_dataset)
 
 real_summary <- real_dataset |>
   summarized_dataset()
